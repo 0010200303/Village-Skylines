c7e0b0c84770b0d197dd8f89590b15d4bb0e881d
0010200303 <jonas.schoenbrodt@gmail.com>
2024-02-02 15:07:40 +0100

    Update senior age

--- constants.py
+++ constants.py
@@ -4,7 +4,7 @@ Some Constants
 __author__ = "8293677, Schoenbrodt, 8288950, Haas"
 
 ADULT_AGE = 6570                # 18 * 365
-SENIOR_AGE = 25550              # 70 * 365
+SENIOR_AGE = 29200              # 80 * 365
 MAX_AGE = 43800                # 120 * 365
 
 MIN_HAPPINESS = 15.0
